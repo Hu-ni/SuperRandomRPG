261aab7c07ffedd28120b1dbb71be6e10fb3231f
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-16 20:43:12 +0900

    feat: Skill 클래스 생성

---
+++ SuperRandomRPG/Models/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperRandomRPG.Models
+{
+    internal class Skill
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; } 
+    }
+
+}
