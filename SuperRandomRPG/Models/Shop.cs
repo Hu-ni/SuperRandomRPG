e9e54569674818db05c1891bb4e2706ca2cf967c
kimyoungsu01 <kysmb200@gmail.com>
2025-07-16 10:31:27 +0900

    Update Shop.cs

--- SuperRandomRPG/Models/Shop.cs
+++ SuperRandomRPG/Models/Shop.cs
@@ -8,7 +8,7 @@ namespace SuperRandomRPG.Models
 {
     public class Reward
     {
-        public int Exp { get; set; }
+        public int Item { get; set; }
         public int Money { get; set; }
     }
 }
