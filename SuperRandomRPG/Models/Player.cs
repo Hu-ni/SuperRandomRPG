4db92bd6f9b421e919d4e14186967a64b1fb6a60
bABCDd <rlatkdrbs6845@gmail.com>
2025-07-17 20:31:46 +0900

    fix: PlayerJob

--- SuperRandomRPG/Models/Player.cs
+++ SuperRandomRPG/Models/Player.cs
@@ -12,7 +12,8 @@ namespace Team_SRRPG.Model
     {
         Warrior,
         Mage,
-        Archer
+        Archer,
+        Gambler
     }
 
     public class Player
@@ -26,20 +27,79 @@ namespace Team_SRRPG.Model
         public int Mana { get; set; } // 플레이어 마나    
         public int Gold { get; set; } // 플레이어 소지 금액    
         public int Luck { get; set; } // 플레이어 행운    
- 
 
-        public Player(string name, Job job, Status status, int level = 1, int exp = 0, int health = 100, int mana = 50, int gold = 1000, int luck = 0)
+
+        public Player(string name, Job job, int level = 1, int exp = 0, int gold = 1000)
         {
             Name = name;
             Job = job;
-            Status = status;
             Level = level;
             Experience = exp;
-            Health = health;
-            Mana = mana;
             Gold = gold;
-            Luck = luck;
+
+            switch (job)
+            {
+                case Job.Warrior:
+                    Status = new Status { Attack = 15, Defense = 12, Health = 120, Mana = 120 };
+                    Mana = Status.Mana;
+                    Luck = 0;
+                    Health = Status.Health; // 초기체력을 Status에서 가져옴
+                    break;
+                case Job.Mage:
+                    Status = new Status { Attack = 10, Defense = 8, Health = 100, Mana = 150 };
+                    Mana = Status.Mana;
+                    Luck = 0;
+                    Health = Status.Health; 
+                    break;
+                case Job.Archer:
+                    Status = new Status { Attack = 12, Defense = 10, Health = 110, Mana = 100 };
+                    Mana = Status.Mana;
+                    Luck = 0;
+                    Health = Status.Health; 
+                    break;
+                case Job.Gambler:
+                    Status = new Status { Attack = 8, Defense = 6, Health = 90, Mana = 80 };
+                    Mana = Status.Mana;
+                    Luck = 10;
+                    Health = Status.Health; 
+                    break;
+            }
         }
+
+        public void LevelUp()
+        {
+            Level++;
+            Experience = 0; // 레벨업 시 경험치 초기화
+            switch(Job)
+            {
+                case Job.Warrior:
+                    Status.Attack += 5;
+                    Status.Defense += 3;
+                    Status.Health += 10;
+                    Status.Mana += 5;
+                    break;
+                case Job.Mage:
+                    Status.Attack += 4;
+                    Status.Defense += 2;
+                    Status.Health += 8;
+                    Status.Mana += 10;
+                    break;
+                case Job.Archer:
+                    Status.Attack += 3;
+                    Status.Defense += 2;
+                    Status.Health += 7;
+                    Status.Mana += 5;
+                    break;
+                case Job.Gambler:
+                    Status.Attack += 2;
+                    Status.Defense += 1;
+                    Status.Health += 5;
+                    Status.Mana += 3;
+                    Luck += 2; // 도박꾼은 레벨업 시 행운 증가
+                    break;
+            }                
+        }
+
         public void OpenStatus(Inventory Inventory)
         {
             int bonusAttack = Inventory.Items.Where(item => item.isEquiped).Sum(item => item.Status.Attack);
